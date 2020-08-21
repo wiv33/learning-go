3cccb1fe7769bc9721e90da3824684bbe96ef845
ps <wiv821253@gmail.com>
2020-08-21 23:57:43 +0900

    add echo demo

---
+++ http-exam/echo-demo.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	dump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+		return
+	}
+	fmt.Println(string(dump))
+	fmt.Fprintf(w, "<html><body>Hello World!</body></html>")
+
+}
+
+func main() {
+	var httpServer http.Server
+	http.HandleFunc("/", handler)
+	log.Println("start http listening : 18888")
+	httpServer.Addr = ":18888"
+	log.Println(httpServer.ListenAndServe())
+}
