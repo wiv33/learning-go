7bc27aed163fa9c1c1a135a48f8948b31eb84fe8
ps <wiv821253@gmail.com>
2020-08-22 16:09:41 +0900

    Dockerfile, echo-demo.go

--- http-exam/echo-demo.go
+++ http-exam/echo-demo.go
@@ -14,8 +14,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	fmt.Println(string(dump))
-	fmt.Fprintf(w, "<html><body>Hello World!</body></html>")
-
+	fmt.Fprintf(w, "<html><body>%s</body></html>", "Hello World!")
 }
 
 func main() {
